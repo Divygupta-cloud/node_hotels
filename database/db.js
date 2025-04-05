95644260fd02be398acacbfc9c2faa850d2a97e7
Divy Gupta <divy.gupta6076@gmail.com>
2025-04-05 11:28:41 +0530

    Next version of project

--- database/db.js
+++ database/db.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 
 // Define the MongoDB connection string
-
-const mongoURL = 'mongodb://localhost:27017/hotels'; // Replace with your MongoDB connection string
+const mongoURL = 'mongodb://localhost:27017/hotels';
 
 mongoose.connect(mongoURL, {});
 
 // Get the default connection
-// Mongoose will automatically create the database if it doesn't exist
 const db = mongoose.connection;
 
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express'); 
-const app = express();
+const app = express(); // Create an instance of Express
 
 const db = require('./database/db'); // Import the database connection
 
@@ -9,12 +9,12 @@ app.use(bodyParser.json()); // Parse JSON request bodies
 
 app.get('/', (req, res) => {
     res.send('Welcome to my Hotel!');
-});
+});// Define a simple route for the root URL
 
 const personRoutes = require('./routes/personRoutes'); // Import the person routes
 app.use('/person', personRoutes); // Use the person routes for /person endpoint
 
-const menuRoutes = require('./routes/menuRoutes'); // Import the MenuItem model
+const menuRoutes = require('./routes/menuRoutes'); // Import the MenuItem Routes
 app.use('/menu', menuRoutes); // Use the menu routes for /menu endpoint
 
 app.listen(3000, () => {
